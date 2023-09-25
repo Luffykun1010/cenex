debdc31e8e8f5f2876b841daf88e88e0127c488f
Luffykun1010 <muhammedsuhusuhail@gmail.com>
2023-09-25 13:36:14 +0530

    cenex

--- cenexfront/views.py
+++ cenexfront/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from .models import UserInfo
+from django.contrib import messages
 def home(request):
     return render(request,'cenexfront/home.html')
 def about(request):
@@ -26,7 +27,17 @@ def signuppage(request):
         return redirect('login')
     return render(request,'cenexfront/signup.html')
 def loginpage(request):
+    if request.method == 'POST':
+        username=request.POST.get('comname')
+        password=request.POST.get('password')
+        user = authenticate(username=username,password=password)
+        if user is not None:    
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,"Invalid credentials")
+            return redirect('login')
     return render(request,'cenexfront/login.html')
 def logoutpage(request):
     logout(request)
-    return redirect('signup')
+    return redirect('login')
